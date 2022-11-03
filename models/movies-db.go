c741fd266e1fb773e07188579016096ee7ea3fa1
tirzasrwn <tirzasrwn@gmail.com>
2022-11-03 12:04:29 +0800

    feat: Serving genre data.

--- models/movies-db.go
+++ models/movies-db.go
@@ -37,6 +37,31 @@ func (m *DBModels) Get(id int) (*Movie, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Get genre, if any
+	query = `select mg.id, mg.movie_id, mg.genre_id, g.genre_name from movies_genres mg left join genres g on (g.id = mg.genre_id) where mg.movie_id = $1`
+
+	rows, _ := m.DB.QueryContext(ctx, query, id)
+	defer rows.Close()
+
+	var genres []MovieGenre
+
+	for rows.Next() {
+		var mg MovieGenre
+		err := rows.Scan(
+			&mg.ID,
+			&mg.MovieID,
+			&mg.GenreID,
+			&mg.Genre.GenreName,
+		)
+		if err != nil {
+			return nil, err
+		}
+		genres = append(genres, mg)
+	}
+
+	movie.MovieGenre = genres
+
 	return &movie, nil
 }
 
