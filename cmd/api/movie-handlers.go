cd354d0f95b14b39487c5aed8f91b33c60f61e6c
tirzasrwn <tirzasrwn@gmail.com>
2022-11-03 17:26:10 +0800

    feat: Getting all movies in json.

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -19,23 +19,27 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	app.logger.Println("id is", id)
 
 	movie, err := app.models.DB.Get(id)
-
-	// movie := models.Movie{
-	// 	ID:          id,
-	// 	Title:       "some title",
-	// 	Description: "some description",
-	// 	Year:        2021,
-	// 	ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-	// 	Runtime:     100,
-	// 	Rating:      5,
-	// 	MPAARating:  "PG-13",
-	// 	CreatedAt:   time.Now(),
-	// 	UpdatedAt:   time.Now(),
-	// }
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request) {
-
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
