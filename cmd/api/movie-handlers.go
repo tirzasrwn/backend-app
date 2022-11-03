aef00e90934bea1662bf8751f3517d05b5e27521
tirzasrwn <tirzasrwn@gmail.com>
2022-11-03 17:32:19 +0800

    feat: Cleaning up movie-handlers.go file.

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -16,7 +16,6 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 		return
 	}
-	app.logger.Println("id is", id)
 
 	movie, err := app.models.DB.Get(id)
 	if err != nil {
@@ -42,4 +41,20 @@ func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request) {
 		app.errorJSON(w, err)
 		return
 	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+
 }
