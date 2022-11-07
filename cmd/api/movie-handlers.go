9d438befa38d9222563d36007eab848fbf13bf1b
tirzasrwn <tirzasrwn@gmail.com>
2022-11-07 16:40:20 +0800

    feat: Getting movies by Genre.

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -30,7 +30,7 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (app *application) getAllMovie(w http.ResponseWriter, r *http.Request) {
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err := app.models.DB.All()
 	if err != nil {
 		app.errorJSON(w, err)
@@ -50,6 +50,21 @@ func (app *application) getAllGenres(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	err = app.writeJSON(w, http.StatusOK, genres, "genres")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) getAllMoviesByGenre(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+	genreID, err := strconv.Atoi(params.ByName("genre_id"))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	movies, err := app.models.DB.All(genreID)
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
 	if err != nil {
 		app.errorJSON(w, err)
 		return
