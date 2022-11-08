79b76ed48030a1249ebe398509f443074c5c1794
tirzasrwn <tirzasrwn@gmail.com>
2022-11-08 17:53:13 +0800

    feat: Sending data to the REST back end.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,5 +13,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:genre_id", app.getAllMoviesByGenre)
 	router.HandlerFunc(http.MethodGet, "/v1/genres", app.getAllGenres)
+	router.HandlerFunc(http.MethodPost, "/v1/admin/editmovie", app.editMovie)
 	return app.enableCORS(router)
 }
