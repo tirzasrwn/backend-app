b96550f5e2dc1b9f5b637ef07886131b07f10959
tirzasrwn <tirzasrwn@gmail.com>
2022-11-09 15:04:00 +0800

    feat: Editing an existing movie.

--- cmd/api/movie-handlers.go
+++ cmd/api/movie-handlers.go
@@ -108,6 +108,14 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var movie models.Movie
+
+	if payload.ID != "0" {
+		id, _ := strconv.Atoi(payload.ID)
+		m, _ := app.models.DB.Get(id)
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
 	movie.ID, _ = strconv.Atoi(payload.ID)
 	movie.Title = payload.Title
 	movie.Description = payload.Description
@@ -121,6 +129,12 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 
 	if movie.ID == 0 {
 		err = app.models.DB.InsertMovie(movie)
+		if err != nil {
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		err = app.models.DB.UpdateMovie(movie)
 		if err != nil {
 			app.errorJSON(w, err)
 			return
--- models/movies-db.go
+++ models/movies-db.go
@@ -179,3 +179,24 @@ func (m *DBModels) InsertMovie(movie Movie) error {
 	}
 	return nil
 }
+
+func (m *DBModels) UpdateMovie(movie Movie) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	stmt := `update movies set title = $1, description = $2, year = $3, release_date = $4, runtime = $5, rating = $6, mpaa_rating = $7, updated_at = $8 where id = $9`
+	_, err := m.DB.ExecContext(ctx, stmt,
+		movie.Title,
+		movie.Description,
+		movie.Year,
+		movie.ReleaseDate,
+		movie.Runtime,
+		movie.Rating,
+		movie.MPAARating,
+		movie.UpdatedAt,
+		movie.ID,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
