b178ad68d019ccfd9795a8feebc2f0898c2c1610
tirzasrwn <tirzasrwn@gmail.com>
2022-11-11 10:38:59 +0800

    typo: Access-Control-Headers to Access-Control-Allow-Headers.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -14,7 +14,7 @@ import (
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Headers", "Content-Type,Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 		next.ServeHTTP(w, r)
 	})
 }
